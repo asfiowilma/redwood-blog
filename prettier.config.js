7e30bf764deae754a46b7d8043c4fce1ea34d227
asfiowilma <asfiolithaw@yahoo.co.id>
2022-07-17 14:54:21 +0700

    🎉 initial commit

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,8 @@
 // https://prettier.io/docs/en/options.html
+<<<<<<< HEAD
+=======
+/** @type {import('prettier').RequiredOptions} */
+>>>>>>> c4814a0 (🎉 initial commit)
 module.exports = {
   trailingComma: 'es5',
   bracketSpacing: true,
@@ -8,9 +12,15 @@ module.exports = {
   arrowParens: 'always',
   overrides: [
     {
+<<<<<<< HEAD
       files: 'Routes.js',
       options: {
         printWidth: 200,
+=======
+      files: 'Routes.*',
+      options: {
+        printWidth: 999,
+>>>>>>> c4814a0 (🎉 initial commit)
       },
     },
   ],
