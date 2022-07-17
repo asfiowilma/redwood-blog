62fd9e5976b71524267a244985184fa9cb55164a
asfiowilma <asfiolithaw@yahoo.co.id>
2022-07-17 17:03:09 +0700

    ✨ add tailwind

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -7,7 +7,6 @@ const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
       <h1>HomePage</h1>
       <p>
         Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
