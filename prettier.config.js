e47d206af18b8345ad759660b35ecd2803fb957f
asfiowilma <asfiolithaw@yahoo.co.id>
2022-07-17 18:14:12 +0700

    🔀 merge with tutorial example repo

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,4 @@
 // https://prettier.io/docs/en/options.html
-<<<<<<< HEAD
-=======
-/** @type {import('prettier').RequiredOptions} */
->>>>>>> c4814a0 (🎉 initial commit)
 module.exports = {
   trailingComma: 'es5',
   bracketSpacing: true,
@@ -12,15 +8,9 @@ module.exports = {
   arrowParens: 'always',
   overrides: [
     {
-<<<<<<< HEAD
       files: 'Routes.js',
       options: {
         printWidth: 200,
-=======
-      files: 'Routes.*',
-      options: {
-        printWidth: 999,
->>>>>>> c4814a0 (🎉 initial commit)
       },
     },
   ],
--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -8,12 +8,13 @@ interface ArticleProps {
 
 const Article = ({ article }: ArticleProps) => {
   return (
-    <article key={article.id}>
-      <h2>
-        <Link to={routes.article({ id: article.id })}>{article.title}</Link>
-      </h2>
-      <p>{article.body}</p>
-      <small>Posted at: {article.createdAt}</small>
+    <article>
+      <header>
+        <h2 className="text-xl text-rose-700 font-semibold">
+          <Link to={routes.article({ id: article.id })}>{article.title}</Link>
+        </h2>
+      </header>
+      <div className="mt-2 text-gray-900 font-light">{article.body}</div>
     </article>
   )
 }
